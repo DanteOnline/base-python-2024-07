55b17ae69501c4d967c6ce4df1450ba85f0d29cd
DanteOnline <iamdanteonline@gmail.com>
2025-01-14 21:31:49 +0300

    add coverage

--- test_step_1.py
+++ test_step_1.py
@@ -4,7 +4,23 @@ def test_index(client):
     assert response.text == '"Hello, World!"'
 
 
-def test_sum(client):
+def test_json(client):
     response = client.get("/json")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello, World in JSON!"}
+
+
+def test_html(client):
+    expected = """
+    <html>
+        <head>
+            <title>Hello, World!</title>
+        </head>
+        <body>
+            <h1>Hello, World in HTML!</h1>
+        </body>
+    </html>
+    """
+    response = client.get("/html")
+    assert response.status_code == 200
+    assert response.text == expected
